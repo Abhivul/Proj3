7e31dab6ea54c2e5493f330181a19e40712b4ec7
Abhishek <Abhishek@192.168.1.13>
2020-01-04 15:26:53 +0530

    I am pushing the whole project

---
+++ Proj3/src/Proj3/Proj3.java
@@ -0,0 +1,20 @@
+package Proj3;
+
+public class Proj3 {
+	
+	Proj3() {
+		
+		System.out.println("Proj3");
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		new Proj3();
+		
+		
+
+	}
+
+}
